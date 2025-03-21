d29cec3e5a01366157893cf37e555fd03e410ded
linqinzhpng <2952868313@qq.com>
2025-03-21 17:58:06 +0800

    feat: 添加页面菜单

--- dev/src/index.tsx
+++ dev/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './theme/canon/index.less'
-import { FebRoutes } from './route';
+import { FebRoutes } from './main';
 import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
---
+++ dev/src/main/pages/quickstart.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export const QuickStart = () => {
+    return <div></div>
+}
